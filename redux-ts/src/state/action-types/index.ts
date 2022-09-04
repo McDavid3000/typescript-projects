d77e48a37dbcd273b0948a85ac738b022e731c99
McDavid3000 <54386026+McDavid3000@users.noreply.github.com>
2022-09-04 14:41:03 +1200

    Add React + Redux app using TS

---
+++ redux-ts/src/state/action-types/index.ts
@@ -0,0 +1,6 @@
+// Eliminates using raw strings which is clearer and reduces risk of typos
+export enum ActionType {
+  SEARCH_REPOSITORIES = 'search_repositories',
+  SEARCH_REPOSITORIES_SUCCESS = 'search_repositories_success',
+  SEARCH_REPOSITORIES_ERROR = 'search_repositories_error',
+}
