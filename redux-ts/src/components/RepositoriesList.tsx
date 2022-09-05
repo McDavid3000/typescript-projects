be40d7c70a89c3d53734227167a928d055ca1a4f
McDavid3000 <54386026+McDavid3000@users.noreply.github.com>
2022-09-05 18:25:04 +1200

    Add useActions.ts * returns bound dispatch and actionCreators * implement in RepositoriesList.tsx

--- redux-ts/src/components/RepositoriesList.tsx
+++ redux-ts/src/components/RepositoriesList.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react';
+import { useActions } from '../hooks/useActions';
+import { actionCreators } from '../state';
 
 const RepositoriesList: React.FC = () => {
   // Array destructuring
   const [term, setTerm] = useState('');
+  const { SearchRepositories } = useActions();
 
   const onSubmit: React.FormEventHandler<HTMLFormElement> = (event) => {
     event.preventDefault();
+
+    // Not an ideal line
+    SearchRepositories(term);
   };
 
   return (
