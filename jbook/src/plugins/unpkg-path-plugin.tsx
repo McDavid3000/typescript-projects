f9ca54a402a7a113c1713b0b9e74bc1c9432b2dc
McDavid3000 <54386026+McDavid3000@users.noreply.github.com>
2022-09-21 09:12:21 +1200

    Refactor unpackage plugin * Remove if statements and use onResolve to filter for handling files

--- jbook/src/plugins/unpkg-path-plugin.tsx
+++ jbook/src/plugins/unpkg-path-plugin.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm';
 import axios from 'axios';
 import localForage from 'localforage';
+import path from 'path';
 
 const fileCache = localForage.createInstance({
   name: 'filecache',
@@ -10,22 +11,22 @@ export const unpkgPathPlugin = (inputCode: string) => {
   return {
     name: 'unpkg-path-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log('onResolve', args);
-        if (args.path === 'index.js') {
-          return { path: args.path, namespace: 'a' };
-        }
+      // Handle root entry file of index.js
+      build.onResolve({ filter: /(^index\.js$)/ }, () => {
+        return { path: 'index.js', namespace: 'a' };
+      });
 
-        // Handling of relative files
-        if (args.path.includes('./') || args.path.includes('../')) {
-          return {
-            namespace: 'a',
-            path: new URL(
-              args.path,
-              'https://unpkg.com' + args.resolveDir + '/'
-            ).href,
-          };
-        }
+      // Handling of relative file paths in a module
+      build.onResolve({ filter: /^\.+\// }, (args: any) => {
+        return {
+          namespace: 'a',
+          path: new URL(args.path, 'https://unpkg.com' + args.resolveDir + '/')
+            .href,
+        };
+      });
+
+      //Handle main file of a module
+      build.onResolve({ filter: /.*/ }, async (args: any) => {
         return {
           namespace: 'a',
           path: `https://unpkg.com/${args.path}`,
